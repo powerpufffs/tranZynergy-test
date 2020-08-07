56a3f8d957c2647e3503a8704cba602f640091a8
Z <asterixandisaac@gmail.com>
2020-08-07 02:04:32 -0600

    added a 404 to client side routes

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,25 @@
 import React from "react";
 // import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { Router, Redirect } from "@reach/router";
+import { Router, Redirect, Link } from "@reach/router";
 
 import Admin from "../components/Admin";
 import Login from "../components/Login";
 import { AuthProvider, AuthContext } from "../utils/authContext";
 
+const FourOFour = () => {
+  return (
+    <div className="flex flex-col items-center h-screen">
+      <h1 className="text-6xl font-bold text-gray-800 mt-10">404</h1>
+      <h2 className="text-4xl">Page Not Found</h2>
+      <div className="mt-2">
+        <Link to="/">Go Back Home</Link>
+      </div>
+    </div>
+  );
+};
+
 const AuthRoutes = ({ element: Element, ...rest }) => {
   const { isLoggedIn } = React.useContext(AuthContext);
-  console.log(isLoggedIn());
   return isLoggedIn() ? <Element {...rest} /> : <Redirect to="/" noThrow />;
 };
 
@@ -18,6 +29,7 @@ function IndexPage() {
       <Router>
         <Login path="/" />
         <AuthRoutes element={Admin} path="admin" />
+        <FourOFour path="*" />
       </Router>
     </AuthProvider>
   );
